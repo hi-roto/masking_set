eed3f6f62667cd3722f3b32f2a6ef0a8d6318512
hi-roto <hirostudy2020@gmail.com>
2021-11-04 22:47:03 +0900

    Aurora Daily Update

--- daily_update_bi_connected_rds.sh
+++ daily_update_bi_connected_rds.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 
+set -eu
+
 aws_comand_path=$(which aws)
 
 call_get_parameter ()
@@ -19,97 +21,130 @@ change_double_to_single_quotation ()
 
 get_latest_snapshot_rds_identifier () 
 {
-  $aws_comand_path rds describe-db-snapshots \
-  --query "reverse(sort_by(DBSnapshots[?DBInstanceIdentifier==$1],&SnapshotCreateTime))[0].DBSnapshotIdentifier"
+  $aws_comand_path rds describe-db-cluster-snapshots \
+  --query "reverse(sort_by(DBClusterSnapshots[?DBClusterIdentifier==$1],&SnapshotCreateTime))[0].DBClusterSnapshotIdentifier"
 }
 
 # 各パラメータを変数に代入
 ANALYTICS_RDS_IDENTIFIER=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_RDS_IDENTIFIER | reject_double_quotation)
+ANALYTICS_RDS_INSTANCE_IDENTIFIER=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_RDS_INSTANCE_IDENTIFIER | reject_double_quotation)
 ANALYTICS_DB_AVAILABILITY_ZONE=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_DB_AVAILABILITY_ZONE | reject_double_quotation)
 ANALYTICS_DB_SUBNET_GROUP_NAME=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_DB_SUBNET_GROUP_NAME | reject_double_quotation)
 ANYLITICS_DB_VPC_SECURITY_GROUP_ID=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANYLITICS_DB_VPC_SECURITY_GROUP_ID | reject_double_quotation)
 ANALYTICS_DB_PARAMETER_GROUP_NAME=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_DB_PARAMETER_GROUP_NAME | reject_double_quotation)
 
-# マスキングRDSがあれば削除する（初回構築時は除く）
-$aws_comand_path rds describe-db-instances --db-instance-identifier $ANALYTICS_RDS_IDENTIFIER > /dev/null
-
-if [ $? -eq 0 ]; then 
-  $aws_comand_path rds delete-db-instance \
-    --db-instance-identifier $ALYTICS_RDS_IDENTIFIER \
-    --skip-final-snapshot \
-    > /dev/null 2>&1
-
-  if [ $? -eq 0 ]; then
-    echo "success_delete_old_maskingRDS" >> ~/masking_set/masking.log 2>&1
-  else
-    echo "error_delete_old_maskingRDS" >> ~/masking_set/masking.log 2>&1
-  fi
-
-  $aws_comand_path rds wait db-instance-deleted --db-instance-identifier $ALYTICS_RDS_IDENTIFIER
-fi
-
-# 本番RDSからスナップショットを取る
+# 本番AuroraMySQLからスナップショットを取る
 DB_IDENTIFIER=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/DB_IDENTIFIER | change_double_to_single_quotation)
-
 main_snapshot_rds_identifier=$(get_latest_snapshot_rds_identifier $DB_IDENTIFIER)
 main_snapshot_rds_identifier=$(echo $main_snapshot_rds_identifier | reject_double_quotation)
 
 SYNC_DB_IDENTIFIER=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/SYNC_DB_IDENTIFIER | reject_double_quotation)
+SYNC_DB_INSTANCE_IDENTIFIER=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/SYNC_DB_INSTANCE_IDENTIFIER | reject_double_quotation)
 
-# スナップショットからRDSを復元する
-$aws_comand_path rds --no-cli-pager restore-db-instance-from-db-snapshot  \
-  --db-snapshot-identifier $main_snapshot_rds_identifier  \
-  --db-instance-identifier $SYNC_DB_IDENTIFIER  \
+# スナップショットからAuroraを復元する クラスターの復元
+$aws_comand_path rds --no-cli-pager restore-db-cluster-from-snapshot  \
+  --snapshot-identifier $main_snapshot_rds_identifier  \
+  --db-cluster-identifier $SYNC_DB_IDENTIFIER  \
+  --engine "aurora-mysql" \
+  --engine-version "5.7.mysql_aurora.2.08.1"\
   --availability-zone $ANALYTICS_DB_AVAILABILITY_ZONE  \
   --db-subnet-group-name $ANALYTICS_DB_SUBNET_GROUP_NAME  \
   --vpc-security-group-ids $ANYLITICS_DB_VPC_SECURITY_GROUP_ID  \
-  --db-parameter-group-name $ANALYTICS_DB_PARAMETER_GROUP_NAME \
-  --db-instance-class db.t3.micro  \
-  --no-multi-az \
   > /dev/null 
 
 if [ $? -eq 0 ]; then
-    echo "success_snapshot_restore_maskingRDS" >> ~/masking_set/masking.log 2>&1
-else
-    echo "error_snapshot_restore_maskingRDS" >> ~/masking_set/masking.log 2>&1
+    echo "success restore-sync-restore-db-cluster" >> ~/masking_set/masking.log 
 fi
 
-# RDSが作成するまで待機 
-$aws_comand_path rds wait db-instance-available --db-instance-identifier $SYNC_DB_IDENTIFIER
-
-ANALYTICS_DB_PASSWORD=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_DB_PASSWORD | reject_double_quotation)
-
-$aws_comand_path rds modify-db-instance \
-    --db-instance-identifier $SYNC_DB_IDENTIFIER \
-    --master-user-password $ANALYTICS_DB_PASSWORD > /dev/null 
+# インスタンスの復元
+$aws_comand_path rds create-db-instance \
+  --db-cluster-identifier $SYNC_DB_IDENTIFIER \
+  --db-instance-identifier $SYNC_DB_INSTANCE_IDENTIFIER \
+  --db-instance-class db.t3.small \
+  --engine "aurora-mysql" \
+  --availability-zone $ANALYTICS_DB_AVAILABILITY_ZONE \
+  > /dev/null
 
 if [ $? -eq 0 ]; then
-    echo "success_change_restore_maskingRDS_identifire_passwword" >> ~/masking_set/masking.log 2>&1
-else
-    echo "error_change_restore_maskingRDS_identifire_passwword" >> ~/masking_set/masking.log 2>&1
+    echo "success restore-sync-create-db-instance" >> ~/masking_set/masking.log 
 fi
+
+# sync_Auroraが作成するまで待機 
+$aws_comand_path rds wait db-instance-available --db-instance-identifier $SYNC_DB_INSTANCE_IDENTIFIER
+
+ANALYTICS_DB_PASSWORD=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/ANALYTICS_DB_PASSWORD  | reject_double_quotation)
+
+$aws_comand_path rds modify-db-cluster \
+  --db-cluster-identifier $SYNC_DB_IDENTIFIER \
+  --master-user-password $ANALYTICS_DB_PASSWORD \
+  --apply-immediately > /dev/null
 
 sleep 1m
 
+if [ $? -eq 0 ]; then
+    echo "success modify-db-cluster-passwword" >> ~/masking_set/masking.log 
+fi
+
 SYNC_DB_ENDPOINT=$(call_get_parameter /Prod/DokugakuEngineer/Analytics/SYNC_DB_ENDPOINT | reject_double_quotation)
 
-# 復元RDSにSQL文を流し込んでデータをマスキング
+# sync_AuroraにSQL文を流し込んでデータをマスキング
 mysql -h$SYNC_DB_ENDPOINT -uroot -p$ANALYTICS_DB_PASSWORD dokugaku_engineer < ~/masking_set/masking_dayly_query.sql 
 
 if [ $? -eq 0 ]; then
-    echo "success_masking_query" >> ~/masking_set/masking.log 2>&1
-else
-    echo "error_masking_query" >> ~/masking_set/masking.log 2>&1
+    echo "success masking-query" >> ~/masking_set/masking.log 
 fi
 
-# マスキングRDSのDBインスタンス識別子を変更する
-$aws_comand_path rds modify-db-instance \
-    --db-instance-identifier $SYNC_DB_IDENTIFIER \
-    --new-db-instance-identifier $ALYTICS_RDS_IDENTIFIER \
+set +eu
+
+# BIツールと連携済みのAuroraがあれば削除する（初回構築時は除く）
+$aws_comand_path rds describe-db-instances --db-instance-identifier $ANALYTICS_RDS_INSTANCE_IDENTIFIER > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then 
+
+  $aws_comand_path rds delete-db-instance \
+    --db-instance-identifier $ANALYTICS_RDS_INSTANCE_IDENTIFIER \
+    --skip-final-snapshot > /dev/null 
+
+  if [ $? -eq 0 ]; then
+    echo "success deleted-old-db-instance" >> ~/masking_set/masking.log
+  fi
+
+  $aws_comand_path rds wait db-instance-deleted --db-instance-identifier $ANALYTICS_RDS_INSTANCE_IDENTIFIER
+
+  $aws_comand_path rds delete-db-cluster \
+    --db-cluster-identifier $ANALYTICS_RDS_IDENTIFIER \
+    --skip-final-snapshot > /dev/null
+  
+  if [ $? -eq 0 ]; then
+    echo "success deleted-old-db-instance" >> ~/masking_set/masking.log
+  fi  
+  
+  sleep 4m
+
+fi
+
+set -eu
+
+sleep 2m
+
+# 日次更新のAuroraインスタンス識別子を変更する
+
+$aws_comand_path rds modify-db-cluster \
+    --db-cluster-identifier $SYNC_DB_IDENTIFIER \
+    --new-db-cluster-identifier $ANALYTICS_RDS_IDENTIFIER \
     --apply-immediately > /dev/null
 
 if [ $? -eq 0 ]; then
-    echo "Success_change_maskingRDS_identifier" >> ~/masking_set/masking.log 2>&1
-else
-    echo "Error_change_maskingRDS_identifier" >> ~/masking_set/masking.log 2>&1
+    echo "success modify-db-cluster-identifier" >> ~/masking_set/masking.log
+fi  
+  
+sleep 2m
+
+$aws_comand_path rds modify-db-instance \
+    --db-instance-identifier $SYNC_DB_INSTANCE_IDENTIFIER \
+    --new-db-instance-identifier $ANALYTICS_RDS_INSTANCE_IDENTIFIER \
+    --apply-immediately > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Complete Daily Update!!" >> ~/masking_set/masking.log
 fi
--- masking_dayly_query.sql
+++ masking_dayly_query.sql
@@ -1,9 +1,12 @@
 
 use dokugaku_engineer;
 
+BEGIN;
+
 -- usernameのマスキング処理 `new_dummy_id`で記載
 update users set username= replace(username, username, concat('masking_dummy_',id));
 
-
 -- emailのマスキング処理 `new_dummy_email_id`で記載
 update users set email= replace(email, left(email,instr(email,'@')- 1), concat('masking_dummy_email_',id));
+
+COMMIT;
